a8581884031d3f628cc20dd9ff533c34397fbf28
siddharthkumar140206-web <siddharthkumar140206@gmail.com>
2026-02-17 11:13:36 +0530

    [Yash] Added Uc1-WelComeMsg

---
+++ Palindrome/src/PalindromeApp.java
@@ -0,0 +1,35 @@
+public class PalindromeApp {
+    /**
+     * ------------------------------------------------------------
+     * MAIN CLASS – UseCase1PalindromeCheckerApp
+     * ------------------------------------------------------------
+     *
+     * Use Case 1: Application Entry & Welcome Message
+     *
+     * Description:
+     * Entry point of the Palindrome Checker Management System.
+     *
+     * @author yash
+     *
+     * @version 1.0
+     */
+
+    public class UseCase1PalindromeCheckerApp {
+
+        /**
+         * Application entry point.
+         *
+         * @param args Command-line arguments
+         */
+        public static void main(String[] args) {
+
+            System.out.println("Welcome to the Palindrome Checker Management System");
+            System.out.println("Version : 1.0");
+            System.out.println("System initialized successfully.");
+        }
+    }
+
+}
+
+
+
